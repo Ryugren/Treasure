763e483f2b9376482d05c56830692e8344b19c53
ryugren <ryugren@icloud.com>
2019-11-04 03:47:41 +0900

    OculasAsset

---
+++ Assets/Scripts/Recenter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Recenter : MonoBehaviour
+{
+    void Update()
+    {
+        // Rキーで位置トラッキングをリセットする
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OVRManager.display.RecenterPose();
+        }
+    }
+}
