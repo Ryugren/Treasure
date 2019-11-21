986b22f60f2a099eda4caff3231363d1cd7a9aa2
toshi <toshi@ex.com>
2019-11-22 06:19:51 +0900

    UI HP,BeamGauge 試作追加,配置はプランナからの指示待ちで

---
+++ Assets/Scripts/UIScripts/BeamGaugeUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// 
+/// Aボタン押したらビームゲージが削られ使い切ったらゲージ回復,
+/// かつ回復中はビームゲージは削られない(ビームが打てない)。
+///
+
+
+public class BeamGaugeUI : MonoBehaviour
+{
+
+    GameManager.ParameterBase PB = new GameManager.ParameterBase();
+    public float amountcount;
+    public Image BeamUiobj;
+    public bool triger;
+    
+    void Update()
+    {   
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            amountcount = BeamUiobj.fillAmount;
+            if (triger)　{ BeamUiobj.fillAmount -= 1.0f / PB.MaxBeamLimitTime * Time.deltaTime;　}
+        }
+        if(BeamUiobj.fillAmount == 0)
+        {　triger = false;　}
+
+        if (triger == false)
+        { BeamUiobj.fillAmount += 1.0f / PB.MaxBeamEnergyRechargeTime * Time.deltaTime;　}
+        if (BeamUiobj.fillAmount == 1)
+        {　triger = true;　}
+    }
+}
---
+++ Assets/Scripts/UIScripts/HpGaugeUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpGaugeUI : MonoBehaviour
+{
+    GameManager.ParameterBase PB = new GameManager.ParameterBase();
+    public Image HpGauge;
+   
+    void Update()
+    {
+        HpGauge.fillAmount = PB.Life / PB.MaxLife;
+    }
+}
