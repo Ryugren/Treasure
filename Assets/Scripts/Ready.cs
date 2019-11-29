f482861a5abe7e8c0f2948f77eb905da3d4ea91e
kokuhei8001 <kokuhei8001@gmail.com>
2019-11-29 15:42:10 +0900

    Ready表示と時間制限を実装

---
+++ Assets/Scripts/Ready.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//コクブ制作
+//Date : 11/29
+
+public class Ready : MonoBehaviour
+{
+    //入力操作の受付
+    public bool isContollorl = false;
+
+    //Debug用
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        IsReadyToEnd();
+    //    }
+    //}
+
+    //Ready状態を終了させる
+    public void IsReadyToEnd()
+    {
+        isContollorl = true;
+        Destroy(this.gameObject);
+    }
+}
