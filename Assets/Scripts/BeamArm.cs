b3f9062bc1a24339f8a656d56be726a3f6a8cdcd
ry-rise-Mac <rise.ry.25691381.sac@ezweb.ne.jp>
2019-12-10 13:47:48 +0900

    ビームの仮設計

--- Assets/Scripts/BeamArm.cs
+++ Assets/Scripts/BeamArm.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class BeamArm : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float distance = 10.5f;
+    Ray ray;
+    RaycastHit hit;
+    private GameObject parentObject;
     void Start()
     {
-        
+        parentObject = transform.parent.gameObject;
+        ray = new Ray(parentObject.transform.position, parentObject.transform.rotation * Vector3.forward);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (inputManager.RC.HandTrigger.Get)
+        {
+            if (inputManager.RC.HandTrigger.Axis > 0.5f)
+            {
+                if (gameManager.BeamEnergyIsUsed() == true)
+                {
+                    BeamFiring();
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// レイを発射
+    /// </summary>
+    void BeamFiring()
+    {
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            hit.collider.GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+        Debug.DrawRay(parentObject.transform.position, parentObject.transform.rotation * Vector3.forward * distance, Color.red);
     }
 }
