55ac18c9a5ba7fe0224f45a438158d734444d539
合屋 佑亮 <ryugren@icloud.com>
2020-01-31 11:30:38 +0900

    マッシュルーム

--- Assets/Scripts/Switchs/SwitchSymbol.cs
+++ Assets/Scripts/Switchs/SwitchSymbol.cs
@@ -6,9 +6,12 @@ public class SwitchSymbol : MonoBehaviour
 {
     [SerializeField]
     private SuperSwitchTarget ssTarget = null;
+    public bool IsBreaked { get; private set; } = false;
     // Start is called before the first frame update
     public void Activate(GameManager gm)
     {
+        if (IsBreaked) return;
         ssTarget.Activate(gm);
+        IsBreaked = true;
     }
 }
