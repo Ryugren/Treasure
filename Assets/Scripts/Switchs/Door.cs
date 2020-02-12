2a5b95f6182fbf3b82703e44555268258dbdaebe
ry-rise-Mac <rise.ry.25691381.sac@ezweb.ne.jp>
2020-02-12 12:26:18 +0900

    ドアのスクリプト追加

--- Assets/Scripts/Switchs/Door.cs
+++ Assets/Scripts/Switchs/Door.cs
@@ -1,7 +1,10 @@
-﻿public class Door : SuperSwitchTarget
+﻿using UnityEngine;
+public class Door : SuperSwitchTarget
 {
+    [SerializeField] private Animator animator;
     public override void Activate(GameManager gm)
     {
         base.Activate(gm);
+        animator.Play("DoorOpen");
     }
 }
