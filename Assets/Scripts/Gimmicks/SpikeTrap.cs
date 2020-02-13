ccdda7d28d00634e7b0f4300685b574c16e16aa3
合屋 佑亮 <ryugren@icloud.com>
2020-02-13 16:33:57 +0900

    スパイクトラップ修正

--- Assets/Scripts/Gimmicks/SpikeTrap.cs
+++ Assets/Scripts/Gimmicks/SpikeTrap.cs
@@ -2,52 +2,18 @@
 public class SpikeTrap : SuperGimmicks
 {
     public bool IsBreaked { get; private set; } = false;
-
-    private RaycastHit hit;
-    private int mask;
-    [SerializeField]
-    private Animator anim = null;
-    private void Awake()
-    {
-        mask = ~(1 << LayerMask.NameToLayer("Cave"));
-    }
-
     public override void Activate(GameManager gm)
     {
         if (IsBreaked) return;
         base.Activate(gm);
         IsBreaked = true;
     }
-    private void Update()
+    private void OnTriggerStay(Collider other)
     {
-        if (Physics.BoxCast(transform.position, Vector3.one * 3f, transform.right, out hit, Quaternion.identity, 20f, mask))
+        if (IsBreaked) return;
+        if (other.gameObject.tag.Contains("Player"))
         {
-            if (hit.collider.tag.Contains("Player"))
-            {
-                if (anim.GetBool("Flag")) return;
-                Debug.Log(hit.collider.name);
-                anim.SetBool("Flag", true);
-                return;
-            }
-        }
-
-        if (!anim.GetBool("Flag")) return;
-        anim.SetBool("Flag", false);
-    }
-    void OnDrawGizmos()
-    {
-        //　Cubeのレイを疑似的に視覚化
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(hit.point, Vector3.one * 3f);
-    }
-    private void OnCollisionStay(Collision collision)
-    {
-        {
-            if (IsBreaked) return;
-            if (collision.gameObject.tag.Contains("Player"))
-            {
-                collision.gameObject.GetComponent<DamageHitter>().Player.Damage(5);
-            }
+            other.gameObject.GetComponent<DamageHitter>().Player.Damage(5);
         }
     }
 }
---
+++ Assets/Scripts/Gimmicks/SpikeTrapMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrapMover : MonoBehaviour
+{
+    [SerializeField]
+    private SpikeTrap parent = null;
+
+    private RaycastHit hit;
+    private int mask;
+    [SerializeField]
+    private Animator anim = null;
+    private void Awake()
+    {
+        mask = ~(1 << LayerMask.NameToLayer("Cave"));
+    }
+    private void Update()
+    {
+        if (!parent.IsBreaked && Physics.BoxCast(transform.position, Vector3.one * 3f, transform.right, out hit, Quaternion.identity, 20f, mask))
+        {
+            if (hit.collider.tag.Contains("Player"))
+            {
+                if (anim.GetBool("Flag")) return;
+                anim.SetBool("Flag", true);
+                return;
+            }
+        }
+        if (!anim.GetBool("Flag")) return;
+        anim.SetBool("Flag", false);
+    }
+    void OnDrawGizmos()
+    {
+        //　Cubeのレイを疑似的に視覚化
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(hit.point, Vector3.one * 3f);
+    }
+}
